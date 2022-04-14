ee9ea7944fe496597e5004a724ae0bceba2b4ba5
ja0nz <mail@ja.nz>
2022-04-14 11:53:21 +0200

    tooling

--- css-gen.js
+++ css-gen.js
@@ -6,58 +6,59 @@ import { chdir } from "node:process";
 import tw from "./tailwind.config.cjs";
 const { theme } = tw;
 
+const [node, cwd, mode] = process.argv;
+const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
+
 chdir("./src/css/");
 
-(async () => {
-  /*
+/*
     Converts the tailwind config elements into custom props.
   */
-  let result = "";
+let result = "";
 
-  const groups = [
-    { key: "colors", prefix: "color" },
-    { key: "spacing", prefix: "space" },
-    { key: "fontSize", prefix: "size" },
-  ];
+const groups = [
+  { key: "colors", prefix: "color" },
+  { key: "spacing", prefix: "space" },
+  { key: "fontSize", prefix: "size" },
+];
 
-  // Add a note that this is auto generated
-  result += `
+// Add a note that this is auto generated
+result += `
       /* VARIABLES GENERATED WITH TAILWIND CONFIG ON ${new Date().toLocaleDateString()}.
       Tokens location: ./tailwind.config.js */
 
       :root {
     `;
 
-  // Loop each group's keys, use that and the associated
-  // property to define a :root custom prop
-  groups.forEach(({ key, prefix }) => {
-    const group = theme[key];
+// Loop each group's keys, use that and the associated
+// property to define a :root custom prop
+groups.forEach(({ key, prefix }) => {
+  const group = theme[key];
 
-    if (!group) {
-      return;
-    }
+  if (!group) {
+    return;
+  }
 
-    Object.keys(group).forEach((key) => {
-      result += `--${prefix}-${key}: ${group[key]};`;
-    });
+  Object.keys(group).forEach((key) => {
+    result += `--${prefix}-${key}: ${group[key]};`;
   });
+});
 
-  // Close the :root block
-  result += `
+// Close the :root block
+result += `
       }
     `;
 
-  // Make the CSS readable to help people with auto-complete in their editors
-  result = format(result, { parser: "scss" });
+// Make the CSS readable to help people with auto-complete in their editors
+result = format(result, { parser: "scss" });
 
-  // Push this file into the CSS dir, ready to go
-  await writeFile(`custom-props.css`, result);
-})();
+// Push this file into the CSS dir, ready to go
+await writeFile(`custom-props.css`, result);
 
 const CUBE = "cube.css";
 rmSync(CUBE, { force: true });
 
-watch(["compositions", "blocks", "utilities"]).on(
+const genCSS = watch(["compositions", "blocks", "utilities"]).on(
   "all",
   async (event, path) => {
     if (event === "add") {
@@ -73,3 +74,8 @@ watch(["compositions", "blocks", "utilities"]).on(
     }
   }
 );
+
+if (mode === "production") {
+  await sleep(500);
+  await genCSS.close();
+}
