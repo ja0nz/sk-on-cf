2469bf9fd7354ab8d360f7c61486ad6bd08abc3a
ja0nz <mail@ja.nz>
2022-04-27 12:58:19 +0200

    test: dev & production env

--- src/lib/api/server.ts
+++ src/lib/api/server.ts
@@ -1,8 +1,10 @@
 import { getIn } from "@thi.ng/paths";
 
+console.log(import.meta.env ?? process.env)
+
 // process.env
 const getEnv = (env: string) =>
-  getIn<{ [k: string]: unknown }>(import.meta.env, [env]);
+  getIn(import.meta.env ?? process.env, [env]);
 
 export const GH_TOKEN = getEnv("VITE_GH_TOKEN"); // github.com -> Settings -> Developer Settings -> Personal access tokens -> token for public repo
 export const REPO = getEnv("VITE_REPO"); // if not set -> prefix JSON paths (f.e. https://github/user/repo -> user/repo is prefix)
